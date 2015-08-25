81f68a1f1fa72f18ec92eef51f535808c14e8f12
GalinaZhang <804988132@qq.com>
2015-08-25 18:32:51 +0800

    Create findMissingRange.cpp

---
+++ findMissingRange.cpp
@@ -0,0 +1,30 @@
+/*找出missing number 范围. 
+number范围[0,99]
+
+input: [0, 1, 2,50 , 52, 75]
+output: "3-49,51,53-74,76-99"
+*/
+
+
+string missingRange(vector<int> &nums){
+	int n = nums.size();
+	string ans;
+	for(int i = 0; i <= n; i++){
+		int start = 0;
+		if(i > 0) start = nums[i-1] + 1;
+		int end = 100;
+		if(i < n) end = nums[i];
+		if(start + 1 <= end){
+			if(start + 1 == end){
+				ans += ",";
+				ans += start;
+			}
+			else{
+				string tmp = "." + start + "-" + to_string(end - 1);
+				ans += tmp;
+			}
+		}
+	}
+	if(ans[0] == ',') ans.erase(ans.begin());
+	return ans;
+}
