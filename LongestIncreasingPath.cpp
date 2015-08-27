d0d77f9b7ea42c28c457d4b43ced5ca8bd9ac44e
GalinaZhang <804988132@qq.com>
2015-08-27 14:49:28 +0800

    Update LongestIncreasingPath.cpp

--- LongestIncreasingPath.cpp
+++ LongestIncreasingPath.cpp
@@ -1,3 +1,11 @@
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
 //二叉树上的最长递增路径
 
 struct TreeNode{
@@ -6,53 +14,53 @@ struct TreeNode{
 	int big;
 	TreeNode *left;
 	TreeNode *right;
-	TreeNode(int x): val(x), small(-1), big(-1){}
+	TreeNode(int x) : val(x), small(-1), big(-1), left(NULL), right(NULL){}
 };
 
 int ans = 0;
 void help(TreeNode *root){
-	if(root == NULL) return 0;
+	if (root == NULL) return;
 	int sm = 0, bg = 0;
-	if(root->left){
-		if(root->left->small == -1) 
+	if (root->left){
+		if (root->left->small == -1)
 			help(root->left);
 
-		if(root->left->val <= root->val){
+		if (root->left->val <= root->val){
 			sm = max(sm, root->left->small);
 			ans = max(ans, sm + 1);
 		}
-			
-		if(root->left->val >= root->val){
+
+		if (root->left->val >= root->val){
 			bg = max(bg, root->left->big);
 			ans = max(ans, bg + 1);
 		}
-			
+
 
 	}
-	if(root->right){
-		if(root->right->small == -1) 
+	if (root->right){
+		if (root->right->small == -1)
 			help(root->right);
 
-		if(root->right->val <= root->val){
+		if (root->right->val <= root->val){
 			sm = max(sm, root->right->small);
 			ans = max(ans, sm + 1);
 		}
-			
-		if(root->right->val >= root->val){
+
+		if (root->right->val >= root->val){
 			bg = max(bg, root->right->big);
 			ans = max(ans, bg + 1);
 		}
-			
+
 	}
-	
+
 	root->small = sm + 1;
 	root->big = bg + 1;
 
-	if(root->left && root->right){
-		if(root->val >= root->left->val && root->val <= root->right->val)
+	if (root->left && root->right){
+		if (root->val >= root->left->val && root->val <= root->right->val)
 			ans = max(ans, root->left->small + root->right->big + 1);
-		if(root->val <= root->left->val && root->val >= root->right->val)
-			ans = max(ans, root->left->big + root->right->small + 1));
+		if (root->val <= root->left->val && root->val >= root->right->val)
+			ans = max(ans, root->left->big + root->right->small + 1);
 
 	}
 	return;
@@ -62,4 +70,15 @@ int LongestIncreasingPath(TreeNode *root)
 {
 	help(root);
 	return ans;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TreeNode *root = new TreeNode(6);
+	root->left = new TreeNode(5); TreeNode *rr = new TreeNode(2);
+	root->right = rr;
+	rr->left = new TreeNode(1);
+	rr->right = new TreeNode(3);
+	cout << LongestIncreasingPath(root);
+	return 0;
 }
