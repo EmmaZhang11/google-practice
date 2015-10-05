9b2ba8dcbfec4d95840b7b23a0174178054691c2
GalinaZhang <804988132@qq.com>
2015-10-05 18:44:41 +0800

    Update isBSTPreorder.cpp

--- isBSTPreorder.cpp
+++ isBSTPreorder.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int v): val(v), left(NULL), right(NULL){}
+};
 
 bool help(vector<int> &nums, int s, int e){
     if(s + 1 >= e) return true; // zero or one or two elements return true;
@@ -19,6 +26,32 @@ bool isBSTPreorder(vector<int> nums){
     return help(nums, 0, nums.size()-1);
 }
 
+TreeNode *reconstruct(vector<int> &nums){
+    if(nums.empty()) return NULL;
+    stack<TreeNode *> st;
+    TreeNode *root = new TreeNode(nums[0]);
+    st.push(root);
+    for(int i = 1; i < nums.size(); i++){
+        TreeNode *cur = new TreeNode(nums[i]);
+        TreeNode *p = st.top();
+        st.pop();
+        if(nums[i] < p->val){
+            p->left = cur;
+            st.push(p);
+            st.push(cur);
+        }else{
+            while(!st.empty() && st.top()->val < nums[i]){
+                p = st.top();
+                st.pop();
+            }
+            p->right = cur;
+            //st.push(p); //pay attention to this palace
+            st.push(cur);
+        }
+    }
+    return root;
+}
+
 int main()
 {
     vector<int> nums={1};
@@ -29,5 +62,8 @@ int main()
     cout << isBSTPreorder(nums) << endl;
     nums={2,0,1,-1,3,7};
     cout << isBSTPreorder(nums) << endl;
+    
+    nums= {5,3,1,4,8,6,7,9};
+    TreeNode *root = reconstruct(nums);
     return 0;
 }
