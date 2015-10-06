77c3164c4fdc8bf9f495700378ef430bf2001c9c
GalinaZhang <804988132@qq.com>
2015-10-06 22:28:31 +0800

    Update and rename LongestInterval.cpp to maxji.cpp

--- maxji.cpp
+++ maxji.cpp
@@ -25,6 +25,28 @@ int LongestInterval(vector<int> nums){
     return ans;
 }
 
+int maxji2(vector<int> nums) {
+	int n = nums.size();
+	vector<int> leftMin(n, nums[0]);
+	vector<int> rightMax(n, nums[n-1]);
+	int res = 0;
+	for(int i = 1; i < n; i++) {
+		leftMin[i] = min(leftMin[i-1], nums[i]);
+	}
+	for(int j = n - 2; j >= 0; j--) {
+		rightMax[j] = max(rightMax[j+1], nums[j]]);
+	}
+	int i = 0; int j = 0;
+	while(i < n && j < n) {
+		if(rightMax[j] > leftMin[i]) {
+			res = max(res, j - i);
+			j++;
+		} else
+			i++;
+	}
+	return res;
+}
+
 int main()
 {
     vector<int> nums = {3,5,4,2,1,7};
