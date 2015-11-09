8a4704e49d39a960e82c4b75d3dd2fa1f57db5a6
GalinaZhang <804988132@qq.com>
2015-11-09 14:54:14 +0800

    Update validTree.cpp

--- validTree.cpp
+++ validTree.cpp
@@ -9,6 +9,7 @@ public:
         return find(fa, fa[k]);
     }
     bool validTree(int n, vector<pair<int, int>>& edges){
+        //Union -find
         vector<int> fa(n, -1);
         for(auto p : edges){
             int x = find(fa, p.first);
@@ -18,6 +19,38 @@ public:
         }
         return n-1 == edges.size();
     }
+    
+    bool hasCycle(vector<vector<int>> &g, vector<int> &v, int k, int p){
+        //dfs 使用后向边和parent
+        if(v[k] == 1) return true;
+        v[k] = 1;
+        for(int i : g[k]){
+            if(i != p && hasCycle(g, v, i, k))
+                return true;
+        }
+        v[k] = 2;
+        return false;
+    }
+    
+    bool validTree2(int n, vector<pair<int, int>>& edges){
+        //dfs
+        vector<vector<int>> g(n);
+        vector<int> v(n, 0);
+        for(pair<int,int> p : edges){
+            g[p.first].push_back(p.second);
+            g[p.second].push_back(p.first);
+        }
+        
+        //cycle
+        if(hasCycle(g, v, 0, -1))
+            return false;
+        //connected
+        for(int i = 0; i < n; i++)
+            if(v[i] != 2)
+                return false;
+        return true;
+    }
+ 
 };
 
 int main()
@@ -25,10 +58,10 @@ int main()
     Solution s;
     int n = 5;
     vector<pair<int, int>> edges = {make_pair(0, 1), make_pair(0, 2), make_pair(0, 3), make_pair(1,4)};
-    bool r = s.validTree(n, edges); //true
+    cout << s.validTree2(n, edges); //true
     edges = {make_pair(0, 1), make_pair(1, 2), make_pair(2, 3), make_pair(1, 3), make_pair(1, 4)};
-    r = s.validTree(n, edges); // false, has cycle
+    cout << s.validTree2(n, edges); // false, has cycle
     edges = {make_pair(0, 1), make_pair(1, 2), make_pair(3, 4)};
-    r = s.validTree(n, edges); //false, not connected
+    cout << s.validTree2(n, edges); //false, not connected
     return 0;
 }
